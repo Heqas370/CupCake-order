685333d9970393ecb7b23c93fd649e99ddd11c2b
Adam <adamh07999@gmail.com>
2026-02-06 10:21:02 +0100

    Initial Commit

---
+++ CupCake order/CupCake_orderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupCake_orderApp.swift
+//  CupCake order
+//
+//  Created by Adam Herman on 06/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct CupCake_orderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
